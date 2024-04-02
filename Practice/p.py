eb84c71e68afefd934f00e947d8c022e885dd8e8
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-04-02 22:31:13 +0530

    Pandas_Module_Practice

---
+++ Practice/p.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import numpy as np
+ 
+# # Creating empty series 
+ser = pd.Series()
+print("Pandas Series: ", ser) 
+ 
+# # simple array 
+data = np.array(['d','e','v']) 
+ser = pd.Series(data) 
+# breakpoint() 
+print("Pandas Series : ", ser)

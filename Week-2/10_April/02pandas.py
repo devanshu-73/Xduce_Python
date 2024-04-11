c674b12f511694531d5d050751ac38b0c2fd937b
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-04-11 14:32:41 +0530

    ..

--- Week-2/10_April/02pandas.py
+++ Week-2/10_April/02pandas.py
@@ -1,5 +1,15 @@
+# Pandas :
+# 1) Getting Data Using Single Condition
+# 2) Getting Data Using Multiple Condition : use &(and),|(or)
+
+
 import pandas as pd
+csv_data = pd.read_csv('data.csv')
 
+# Getting Data : Single Condition
 
+print(csv_data[csv_data['Gender']== 'Male'])
 
-print('Hello Pandas..')
+# Getting Data : Multiple Condition
+
+print(csv_data[(csv_data['Gender']== 'Male') & (csv_data['Age'] >= 30)])

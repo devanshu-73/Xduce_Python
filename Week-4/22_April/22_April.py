c89a11dcf1fd6560e98e6d4469c185985963514c
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-04-22 21:40:21 +0530

    Python_MySql

--- Week-4/22_April/22_April.py
+++ Week-4/22_April/22_April.py
@@ -2,11 +2,21 @@ from Connection import Connection_to_mysql
 
 connection = Connection_to_mysql()
 
-print(connection)
+# print(connection)
 if connection:
     print('Database Connected Successfully...')
 
-    
+    cursor = connection.cursor()
+
+    query = 'select * from table1;'
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
+    print('Connection Closed!...')
 
 else:
     print('Database Connection Failed!!')

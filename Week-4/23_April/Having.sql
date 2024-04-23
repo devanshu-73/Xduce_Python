6509d57d87c14b4ec7e93a80baae8f7ac38f3a89
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-04-23 22:13:49 +0530

    Having

---
+++ Week-4/23_April/Having.sql
@@ -0,0 +1,7 @@
+use practice;
+
+-- sequence : FROM > WHERE > GROUP BY > HAVING > ORDER BY
+-- WHERE clause : filters rows before any grouping or aggregation occurs.
+-- HAVING clause : filters groups of rows after they have been grouped and aggregated.
+
+-- Count of People in Each City with More than 30 Years of Age:

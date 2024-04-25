822a1d49d727737acb98e7629a4cdce5be3e7160
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-04-25 19:06:42 +0530

    Joins

--- Week-4/23_April/Joins.sql
+++ Week-4/23_April/Joins.sql
@@ -1,4 +1,4 @@
-use sakila;
+use practice;
 
 select * from film_category;
 -- --------------------------------------------------------
@@ -22,7 +22,44 @@ select film.film_id,film.title,film_category.category_id from film left join fil
 select film.film_id,film.title,film_category.category_id from film right join film_category on film.film_id = film_category.film_id;
 
 -- Full (Outer) Join : 'table1 FULL JOIN table2'
-select * from actor full join film_actor on  actor.actor_id = film_actor.actor_id;
+select film.film_id,film.title,film_category.category_id from film full join film_category on film.film_id = film_category.film_id;
 
 -- --------------------------------------------------------
 
+use practice;
+
+CREATE TABLE employees (
+    emp_id INT,
+    emp_name VARCHAR(50),
+    dept_name VARCHAR(50)
+);
+
+CREATE TABLE departments (
+    dept_id INT,
+    dept_name VARCHAR(50)
+);
+
+-- drop table departments;
+
+INSERT INTO employees VALUES
+(1, 'Alice', 'Sales'),
+(2, 'Bob', 'Marketing'),
+(3, 'Charlie', 'HR');
+
+INSERT INTO departments VALUES
+(1, 'Sales'),
+(2, 'Marketing'),
+(3, 'Finance');
+
+select dept_name from employees;
+select dept_name from departments;
+
+Select emp.dept_name from employees emp FULL JOIN 
+departments dept on emp.dept_name = dept.dept_name;
+
+SELECT emp_id, emp_name, departments.dept_id, departments.dept_name
+FROM employees
+FULL JOIN departments ON employees.dept_name = departments.dept_name;
+
+desc departments;
+desc employees;

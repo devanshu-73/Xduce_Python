9cab1294674392f3eb7641e1ad526f732667bdf7
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-04-29 21:48:11 +0530

    PYODBC_MSSQL

--- Week-5/MSSQL_Python/01_PYODBC.py
+++ Week-5/MSSQL_Python/01_PYODBC.py
@@ -1,24 +1,27 @@
 import pyodbc
-import pandas
 
 SERVER = r'DEVANSHU-DTS\SQL'
 DATABASE = 'DETAILS'
 USERNAME = 'sa'
 PASSWORD = 'XDuce@123'
 
-
 try:
     connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
     conn = pyodbc.connect(connectionString)
-    print('Connected SuccessFully....')
+    print('Connected successfully....')
+    
     cursor = conn.cursor()
+
     cursor.execute(
-        "CREATE TABLE CLASS (SID INT IDENTITY(101,1) PRIMARY KEY,SNAME VARCHAR(20));"
+        "IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'CLASS') CREATE TABLE CLASS (SID INT IDENTITY(101,1) PRIMARY KEY,SNAME VARCHAR(20));"
     )
-    cursor.execute(
-        "INSERT INTO CLASS (SNAME) VALUES ('DEVANSHU'),('MOHSIN');"
-    )
-    cursor.commit()
-    print('SuccessFully Executed....')
+    query = cursor.execute("SELECT * FROM CLASS;")
+    data = query.fetchall()
+    
+    # "INSERT INTO CLASS (SNAME) VALUES ('DEVANSHU'),('MOHSIN');"
+    print(data)
+    conn.commit()
+    print('Successfully executed....')
+    
 except Exception as e:
     print("Error:", e)

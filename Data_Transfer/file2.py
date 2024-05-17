18b41214407e565e9ab57b5066d300c107be8156
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-05-17 14:35:15 +0530

    File_2

---
+++ Data_Transfer/file2.py
@@ -0,0 +1,7 @@
+
+def func2(data_list):
+    print("==================Hello===========================")
+    print(type(data_list))
+    data_file1 = data_list
+    print(data_file1,type(data_file1))
+    return [data_file1]

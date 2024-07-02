24b4a7844da38b2d431d945cd776737e31de2404
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-07-02 22:57:03 +0530

    Sample

--- Practice/2logging.py
+++ Practice/2logging.py
@@ -7,5 +7,6 @@ for i in range(1, 6):
     print(i)
     logging.info("Counting End SuccessFully!....")
 
+# Just Practice
 # logging.basicConfig(level=logging.DEBUG,
 #                     format='%(asctime)s - %(levelname)s - %(message)s')

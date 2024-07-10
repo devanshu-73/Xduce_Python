63715ac8161fa2148e9ad7252cfa9edd56493b50
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-07-10 17:07:14 +0530

    Updated Code

--- Dragable-column/src/MyDraggableComponent.jsx
+++ Dragable-column/src/MyDraggableComponent.jsx
@@ -10,8 +10,8 @@ function MyDraggableComponent() {
     <Draggable
         onDrag={eventHandler}>
         <div style={{border: "2px solid red", padding: "1rem", width: "5%"}}>
-          {/* <div className="handle">Drag from here</div>
-          <div>This readme is really dragging on the onDrag react event listener...</div> */}
+          <div className="handle">Drag from here</div>
+          <div>This readme is really dragging on the onDrag react event listener...</div>
         </div>
     </Draggable>
   )

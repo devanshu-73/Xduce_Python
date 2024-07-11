37b2e42cb77649fa03aeaf912146eae8a78e53a7
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-07-11 17:03:59 +0530

    Updated code

---
+++ sample-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
+
+export default App;

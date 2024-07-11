4402a436558c3c80e040073e0c0f930299eae398
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-07-11 16:09:44 +0530

    [UPD] : Pyspark

--- Practice/Pyspark/file01.py
+++ Practice/Pyspark/file01.py
@@ -1,4 +1,11 @@
+import findspark
+findspark.init()
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("DataExplorers").getOrCreate()
-print(spark)
+spark = SparkSession.builder \
+    .appName("CSV Reader") \
+    .getOrCreate()
+
+df = spark.read.csv('dataset.csv',header = True, inferSchema = True)
+
+df.show()
